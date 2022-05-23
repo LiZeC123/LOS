e8c07aca918bf7bd7de4de8e0743912b399f05f9
LiZeC <tomgreem123@gmail.com>
2022-05-23 09:55:38 +0800

    补充输出相关测试内容

--- kernel/main.c
+++ kernel/main.c
@@ -1,9 +1,19 @@
 #include "print.h"
 
+void put_str(const char* str, int len);
+
+// 注意: 由于入口点是强制指定的地址, 因此main必须是这个文件中的第一个函数
+// 从而在text段中main的代码才能处于开始位置
 int main() {
-  const char *str = "Hello From Kernel.\n 12\b3";
-  for (int i = 0; i < 24; i++) {
-    put_char(str[i]);
+  char str[] = "Hello From Kernel.";
+  const int L = sizeof(str) / sizeof(char) - 1;
+  put_str(str, L);
+  next_line();
+
+  for (int i = 0; i < 30; i++) {
+    put_str("Line ", 5);
+    put_char('1' + i % 10);
+    next_line();
   }
 
   while (1) {
@@ -11,3 +21,11 @@ int main() {
 
   return 0;
 }
+
+void put_str(const char* str, int len) {
+  for (int i = 0; i < len; i++) {
+    put_char(str[i]);
+  }
+}
+
+void next_line() { put_char('\n'); }
