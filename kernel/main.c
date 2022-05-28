541c8aabedf439430844ba18c70c7847c12b9ab0
LiZeC <tomgreem123@>
2022-05-28 15:33:40 +0000

    分析入口点位置, 从而避免强制main函数为第一个函数

--- kernel/main.c
+++ kernel/main.c
@@ -1,9 +1,13 @@
 #include "print.h"
 
-void put_str(const char* str, int len);
+void put_str(const char* str, int len) {
+  for (int i = 0; i < len; i++) {
+    put_char(str[i]);
+  }
+}
 
-// 注意: 由于入口点是强制指定的地址, 因此main必须是这个文件中的第一个函数
-// 从而在text段中main的代码才能处于开始位置
+void next_line() { put_char('\n'); }
+
 int main() {
   char str[] = "Hello From Kernel.";
   const int L = sizeof(str) / sizeof(char) - 1;
@@ -21,11 +25,3 @@ int main() {
 
   return 0;
 }
-
-void put_str(const char* str, int len) {
-  for (int i = 0; i < len; i++) {
-    put_char(str[i]);
-  }
-}
-
-void next_line() { put_char('\n'); }
